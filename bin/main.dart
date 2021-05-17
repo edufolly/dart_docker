0fe2ed18015c9af88f80ad68202ec500a29a5974
Eduardo Folly <edufolly@gmail.com>
2021-05-16 23:54:08 -0300

    Initial tests.

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,22 @@
 import 'dart:io';
 
+// import 'package:http/http.dart';
+import 'package:path/path.dart' as p;
+
 ///
 ///
 ///
 void main(List<String> arguments) {
   HttpServer.bind(InternetAddress.anyIPv4, 8080).then((HttpServer server) {
-    server.listen((HttpRequest request) {
-      request.response.write('Hello World.');
-      request.response.close();
+    server.listen((HttpRequest request) async {
+      request.response.write('Hello World - ${p.current} - ${DateTime.now()}');
+
+      // Response response =
+      //     await get(Uri.parse('https://api64.ipify.org?format=json'));
+
+      // request.response.write(response.body);
+
+      await request.response.close();
     });
   });
 }
--- build.sh
+++ build.sh
@@ -1,12 +1,22 @@
 # /bin/bash
-
+set +e
 set -x
 
 docker rm -f main
 
 rm -f bin/main.exe
 
-docker run --name dart_compile -v $(pwd):/app -t -d google/dart
+#####
+
+mkdir -p build/bin/
+cp -Rf bin/ build/bin/
+cp -f *.yaml build/
+
+#####
+
+docker run --name dart_compile -v $(pwd)/build:/app -t -d google/dart
+
+docker exec -w /app dart_compile dart pub get
 
 docker exec dart_compile dart compile exe /app/bin/main.dart
 
@@ -14,7 +24,16 @@ docker stop dart_compile
 
 docker rm dart_compile
 
+#####
+
+cp build/bin/main.exe bin/main.exe
+rm -rf build
+
+#####
+
 docker build -t dart_native .
+
+#####
 
 docker run --name main -p 8080:8080 -d dart_native
 
