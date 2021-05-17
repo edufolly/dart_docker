3679f45389f25be8e4ced92324e8cacbe47e8dfd
Eduardo Folly <edufolly@gmail.com>
2021-05-17 11:25:49 -0300

    Better dockerfile.

--- build.sh
+++ build.sh
@@ -1,39 +1,9 @@
 # /bin/bash
-set +e
 set -x
 
 docker rm -f main
 
-rm -f bin/main.exe
-
-#####
-
-mkdir -p build/bin/
-cp -Rf bin/ build/bin/
-cp -f *.yaml build/
-
-#####
-
-docker run --name dart_compile -v $(pwd)/build:/app -t -d google/dart
-
-docker exec -w /app dart_compile dart pub get
-
-docker exec dart_compile dart compile exe /app/bin/main.dart
-
-docker stop dart_compile
-
-docker rm dart_compile
-
-#####
-
-cp build/bin/main.exe bin/main.exe
-rm -rf build
-
-#####
-
 docker build -t dart_native .
-
-#####
 
 docker run --name main -p 8080:8080 -d dart_native
 
--- dockerfile
+++ dockerfile
@@ -1,10 +1,19 @@
-FROM subfuzion/dart:slim
+FROM google/dart AS dart
 
 WORKDIR /app
 
-ADD ./bin/main.exe main
-# RUN chmod +x main
+ADD pubspec.* /app/
+RUN pub get
+ADD . /app
+RUN pub get --offline
+
+RUN dart compile exe bin/main.dart
+
+FROM subfuzion/dart:slim
+
+COPY --from=dart /app/bin/main.exe /main
+
+CMD []
+ENTRYPOINT [ "./main" ]
 
 EXPOSE 8080
-
-ENTRYPOINT [ "./main" ]
